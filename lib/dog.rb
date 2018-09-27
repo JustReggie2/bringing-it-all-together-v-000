3cb0567603293d81d3b17137f081c53fa5ba8c7f
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-09-27 23:53:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -30,8 +30,10 @@ class Dog
   end
 
   def self.new_from_db(row)
-    dog = Dog.new(row[0], row[1], row[2])
-    dog
+    dog = Dog.new
+    dog.id = row[0]
+    dog.name = row[1]
+    dog.breed = row[2]
   end
 
   # def save
