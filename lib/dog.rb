5dd04e661454c44f30dd4351716eed94451c1d78
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-09-27 23:34:34 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -17,9 +17,17 @@ class Dog
         id INTEGER PRIMARY KEY,
         name TEXT,
         breed TEXT
-      )
+      );
     SQL
 
     DB[:conn].execute(sql)
   end
+
+  def self.drop_table
+    sql = "DROP TABLE dogs;"
+
+
+
+
+
 end
