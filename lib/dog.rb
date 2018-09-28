0b7a845744456af4bf5d97663a0bbe102a2a2558
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-09-28 00:32:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,7 +72,11 @@ class Dog
     DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
-
+  def self.create(name:, breed:)
+    dog = Dog.new(name:, breed:)
+    dog.save
+    dog
+  end
 
 
 end
