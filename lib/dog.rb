8e1db140fc1dc3c145cc00aae3f1664b182ce552
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-09-28 00:54:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -85,8 +85,9 @@ class Dog
       WHERE id = ?
     SQL
 
-    result = DB[:conn].execute(sql, id)[0]
-    Dog.new(id: result[0], name: result[1], breed: result[2])
+    result = DB[:conn].execute(sql, id).map do |row|
+      new_from_db(row)
+    end.first
   end
 
 
